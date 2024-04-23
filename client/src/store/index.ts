6d972eab33390cf606f6943807dbf0278b28807c
Nikolla-L <nlukava2002@gmail.com>
2024-04-23 04:26:49 +0400

    update tsconfigs and add typecheck script

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,11 +1,11 @@
-import { createStore } from "vuex";
-import user from "./modules/user";
-import products from "./modules/products";
+// import { createStore } from "vuex";
+// import user from "./modules/user";
+// import products from "./modules/products";
 
-export default createStore({
-  modules: {
-    user,
-    products,
-  },
-  // ... other state, mutations, actions, and getters
-});
+// export default createStore({
+//   modules: {
+//     user,
+//     products,
+//   },
+//   // ... other state, mutations, actions, and getters
+// });
