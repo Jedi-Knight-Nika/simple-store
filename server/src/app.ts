8e5c955d5388bd717ec2a54e05c329d310736cd1
Nikolla-L <nlukava2002@gmail.com>
2024-04-23 04:55:01 +0400

    changes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,13 @@
-import { startServer } from "./configuration/Server";
 import { Server } from "http";
+
+import { startServer } from "./configuration/Server";
 
 (async () => {
   const server: Server = startServer();
 
   process.on("SIGTERM", () => {
     console.info("SIGTERM signal received.");
+    
     server.close();
   });
 })();
--- server/src/configuration/Server.ts
+++ server/src/configuration/Server.ts
@@ -1,31 +1,34 @@
-import express from 'express';
-import { createExpressServer, RoutingControllersOptions } from 'routing-controllers';
+import express from "express";
+import { createExpressServer, RoutingControllersOptions } from "routing-controllers";
 import * as path from "path";
+// import * as swaggerUiExpress from "swagger-ui-express";
 
 import config from "../config";
+import { Server } from "http";
 
 
 const routingControllersOptions = <RoutingControllersOptions>{
-    routePrefix: '/api/v1',
-    controllers: [__dirname + '/../controller/*'],
-    cors: {
-        origin: '*',
-        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-        preflightContinue: false,
-        optionsSuccessStatus: 204
+  routePrefix: "/api/v1",
+  controllers: [__dirname + "/../controller/*"],
+  cors: {
+    origin: "*",
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  },
+  defaults: {
+    paramOptions: {
+      required: true,
     },
-    defaults: {
-        paramOptions: {
-            required: true
-        }
-    }
+  },
 };
 
-export const startServer = () => {
-    const app = createExpressServer(routingControllersOptions);
-    app.use('/', express.static(path.join(__dirname, '../public')));
+export const startServer = (): Server => {
+  const app = createExpressServer(routingControllersOptions);
+  app.use("/", express.static(path.join(__dirname, "../public")));
 
-    return app.listen(config.PORT, () => {
-        console.info(`Running on port ${config.PORT}`);
-    });
-}
+
+  return app.listen(config.PORT, () => {
+    console.info(`Running on port ${config.PORT}`);
+  });
+};
