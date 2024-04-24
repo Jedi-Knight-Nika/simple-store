9adcd6e99cc8a277f6b8ec62ebbf13e8ead18779
Nikolla-L <nlukava2002@gmail.com>
2024-04-25 00:15:42 +0400

    implement products service

--- server/src/service/product.service.ts
+++ server/src/service/product.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from "../configuration/container";
+import { BaseApiService } from "./base-api.service";
 import {
   Product,
   ProductDetails,
   ProductHttpResponse,
-  ProductImage,
   ProductImageDetails,
   ProductServiceToken,
 } from "../model";
-import { BaseApiService } from "./base-api.service";
 
 @Injectable(ProductServiceToken)
 export class ProductService extends BaseApiService {
@@ -18,24 +17,18 @@ export class ProductService extends BaseApiService {
   public async list(): Promise<Product[]> {
     const result = await this.get<ProductHttpResponse>("products");
 
-    const items = result?.items;
+    const items = result?.items ?? [];
 
-    if (items.length === 0) {
-      return [];
-    }
-
-    return items.map<Product>(
-      (item: ProductDetails): Product => ({
-        id: item.id,
-        name: item.name,
-        price: item.price,
-        images: (item.media?.images as ProductImageDetails[])?.map<ProductImage>((image) => ({
-          id: image.id,
-          isMain: image.isMain,
-          url: image.imageOriginalUrl,
-        })),
-      }),
-    );
+    return items.map((item: ProductDetails): Product => ({
+      id: item.id,
+      name: item.name,
+      price: item.price,
+      images: item.media?.images?.map((image: ProductImageDetails) => ({
+        id: image.id,
+        isMain: image.isMain,
+        url: image.imageOriginalUrl,
+      })) ?? [],
+    }));
   }
 
   public async details(): Promise<ProductDetails | null> {
