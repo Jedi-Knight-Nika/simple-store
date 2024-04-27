98f278c16b9c357536cf6a512d2a9ed14519c9c0
Nikolla-L <nlukava2002@gmail.com>
2024-04-27 11:53:23 +0400

    remove some models

--- server/src/model/index.ts
+++ server/src/model/index.ts
@@ -1,6 +1,3 @@
-export * from "./api.model";
-export * from "./cache.model";
 export * from "./cart.model";
-export * from "./error.model";
 export * from "./product.model";
 export * from "./setting.model";
