39884ab3421b0794522f71e6f6d4a718ad8fbe2b
Nikolla-L <nlukava2002@gmail.com>
2024-04-28 00:40:54 +0400

    implement product controller

--- server/src/controller/product.controller.ts
+++ server/src/controller/product.controller.ts
@@ -1,6 +1,6 @@
 import { Get, JsonController, Param } from "routing-controllers";
-
 import { inject, Injectable } from "../configuration/container";
+
 import { ProductId, ProductServiceToken } from "../model";
 import { ProductService } from "../service";
 
@@ -10,17 +10,17 @@ export class ProductController {
   constructor(@inject(ProductServiceToken) private readonly productService: ProductService) {}
 
   @Get("/ping")
-  ping() {
+  public ping() {
     return this.productService.ping();
   }
 
   @Get()
-  async list() {
+  public async list() {
     return await this.productService.list();
   }
 
   @Get("/:id")
-  async details(@Param("id") id: ProductId) {
+  public async details(@Param("id") id: ProductId) {
     return await this.productService.details(id);
   }
 }
